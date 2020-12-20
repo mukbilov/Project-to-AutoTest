e89c5898d57d43e223f2061ce557ce4bff13296e
Adiz <55481075+mukbilov@users.noreply.github.com>
2020-12-21 01:38:02 +0300

    На проверку!

--- test_product_page.py
+++ test_product_page.py
@@ -78,7 +78,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-
 @pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
