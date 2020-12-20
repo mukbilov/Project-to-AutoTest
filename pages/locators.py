42a35aa3fd9eff095f63ac400451ddb117d26efb
Adiz <55481075+mukbilov@users.noreply.github.com>
2020-12-21 01:45:11 +0300

    На проверку!

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
