b82f0064c53bf50a75e690da784cd0ce7b63f745
Adiz <55481075+mukbilov@users.noreply.github.com>
2020-12-21 01:38:44 +0300

    На проверку!

--- test_main_page.py
+++ test_main_page.py
@@ -21,7 +21,8 @@ class TestLoginFromMainPage():
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
-
+        
+        
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
