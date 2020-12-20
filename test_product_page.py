2775cc749a53ce6ff9cbee5a071fbd927c85642a
Adiz <55481075+mukbilov@users.noreply.github.com>
2020-12-21 02:30:49 +0300

    Убрал точки перед pages

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,6 @@
-from .pages.product_page import ProductPage
-from .pages.login_page import LoginPage
-from .pages.basket_page import BasketPage
+from pages.product_page import ProductPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 import time
 import pytest
 
